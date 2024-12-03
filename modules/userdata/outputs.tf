8681bd8642eba30bf2b595b3d2345f450810ef2e
mathi88sudeep <sudeepsai2020@gmail.com>
2024-12-03 19:17:00 +0530

    Update outputs.tf

--- modules/userdata/outputs.tf
+++ modules/userdata/outputs.tf
@@ -1,3 +1,7 @@
+output "public_key" {
+  value = aws_key_pair.ssh_key.public_key
+}
+ 
 output "user_data" {
   value       = data.cloudinit_config.cloudinit_config.rendered
   description = "User data as string"
